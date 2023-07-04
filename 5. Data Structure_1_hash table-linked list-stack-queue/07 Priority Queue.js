ee77e9c090be1e6ede8abbbd44efe3c958457e25
Kiwi_Lin <opda0887@gmail.com>
2023-07-04 08:56:15 +0800

    7/4 PQ dequeue

--- 5. Data Structure_1_hash table-linked list-stack-queue/07 Priority Queue.js
+++ 5. Data Structure_1_hash table-linked list-stack-queue/07 Priority Queue.js
@@ -39,6 +39,48 @@ class PriorityQueue {
     }
   }
 
+  dequeue() {
+    if (this.values.length == 0) return null;
+    else if (this.values.length == 1) {
+      let removeNode = this.values.pop();
+      return removeNode;
+    }
+
+    // swap two (first & last) nodes, then remove last node
+    let firstNode = this.values[0];
+    this.values[0] = this.values[this.values.length - 1];
+    this.values[this.values.length - 1] = firstNode;
+    let removeNode = this.values.pop(); // the first node
+    // then do "maxHeapify"
+    this.maxHeapify(0);
+    return removeNode;
+  }
+
+  maxHeapify(parentIndex) {
+    let largestIndex;
+    let leftIndex = parentIndex * 2 + 1;
+    let rightIndex = parentIndex * 2 + 2;
+    // find the largest value's index in: parentNode, leftNode, rightNode
+    if (
+      leftIndex <= this.values.length - 1 &&
+      this.values[leftIndex].priority > this.values[parentIndex].priority
+    )
+      largestIndex = leftIndex;
+    else largestIndex = parentIndex;
+    if (
+      rightIndex <= this.values.length - 1 &&
+      this.values[rightIndex].priority > this.values[largestIndex].priority
+    )
+      largestIndex = rightIndex;
+    // swap the data if the largestIndex !== parentIndex
+    if (largestIndex !== parentIndex) {
+      let highPriorityNode = this.values[largestIndex];
+      this.values[largestIndex] = this.values[parentIndex];
+      this.values[parentIndex] = highPriorityNode;
+      this.maxHeapify(largestIndex);
+    }
+  }
+
   printPQ() {
     console.log(this.values);
   }
@@ -50,5 +92,11 @@ myPQ.enqueue("Wash dishes", 3);
 myPQ.enqueue("Learn coding", 5);
 myPQ.enqueue("Go to sleep", 1);
 myPQ.enqueue("Play video games", 2);
+myPQ.enqueue("Do math", 4);
 
+myPQ.printPQ();
+
+console.log(myPQ.dequeue());
+console.log(myPQ.dequeue());
+console.log(myPQ.dequeue());
 myPQ.printPQ();
